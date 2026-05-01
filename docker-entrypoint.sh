adfeef991be1bb9b09f954a01a62e1e595cdf2f8
klutch8ster <42814364+seab4ng@users.noreply.github.com>
2026-05-02 00:47:50 +0300

    chore: remove App name field from About modal and APP_NAME from Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 
-APP_NAME="${APP_NAME:-helm-values-editor}"
 APP_VERSION="${APP_VERSION:-none}"
 
-sed -i "s|__APP_NAME__|${APP_NAME}|g; s|__APP_VERSION__|${APP_VERSION}|g" \
+sed -i "s|__APP_VERSION__|${APP_VERSION}|g" \
   /usr/share/nginx/html/index.html
 
 exec nginx -g "daemon off;"
